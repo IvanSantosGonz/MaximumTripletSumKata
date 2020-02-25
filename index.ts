77cecce7ed08a78cfdcd12a549c75729f098143d
Ivan <talisantos23@hotmail.com>
2020-02-25 20:10:59 +0000

    divide concatenated methods

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 export function maxTriSum(numbers: number[] = []): number {
      let sum: number = 0;
-     [...new Set(numbers)].sort((n1, n2) => n2 - n1).slice(0, 3).forEach(n => sum += n);
+     [...new Set(numbers)]
+         .sort((n1, n2) => n2 - n1)
+         .slice(0, 3)
+         .forEach(n => sum += n);
+     
      return sum
 }
 
